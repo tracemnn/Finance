0151929b9fdb293b8dde5844c9a733c289ef4a64
Buya <tracemnn2@gmail.com>
2023-03-21 15:04:18 +0800

    event listener uusgew

---
+++ app.js
@@ -0,0 +1,25 @@
+var uiController = (function() {
+    
+})();
+
+var financeController = (function() {
+
+})();
+
+var appController = (function(uiController, financeController) {
+
+    var ctrlAddItem = function() {
+        console.log('system ajilla');
+    };
+    
+    document.querySelector('.add__btn').addEventListener('click', function() {
+       ctrlAddItem();
+    });
+
+    document.addEventListener('keypress', function(event) {
+        if(event.keyCode === 13 || event.which === 13 ) {
+            ctrlAddItem();
+        }
+    });
+
+})(uiController, financeController);
